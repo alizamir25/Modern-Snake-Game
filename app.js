6915eca71bc8aa5f3fe557645b57db9c55e973c3
alizamir25 <ali0786zamir@gmail.com>
2024-08-30 14:34:06 +0100

    Patch

--- app.js
+++ app.js
@@ -127,24 +127,24 @@ let KEY={
   ArrowDown: false,
   ArrowLeft: false,
   resetState() {
-    this.ArrowUp = false;
-    this.ArrowRight = false;
-    this.ArrowDown = false;
-    this.ArrowLeft = false;
+    this.ArrowUp=false;
+    this.ArrowRight=false;
+    this.ArrowDown=false;
+    this.ArrowLeft=false;
   },
-  listen() {
+  listen(){
     addEventListener(
       "keydown",
-      (e) => {
-        if (e.key === "ArrowUp" && this.ArrowDown) return;
-        if (e.key === "ArrowDown" && this.ArrowUp) return;
-        if (e.key === "ArrowLeft" && this.ArrowRight) return;
-        if (e.key === "ArrowRight" && this.ArrowLeft) return;
-        this[e.key] = true;
+      (e)=>{
+        if (e.key==="ArrowUp" && this.ArrowDown)return;
+        if (e.key==="ArrowDown" && this.ArrowUp)return;
+        if (e.key==="ArrowLeft" && this.ArrowRight)return;
+        if (e.key==="ArrowRight" && this.ArrowLeft)return;
+        this[e.key]=true;
         Object.keys(this)
-          .filter((f) => f !== e.key && f !== "listen" && f !== "resetState")
-          .forEach((k) => {
-            this[k] = false;
+          .filter((f)=>f!= e.key && f!=="listen" && f!=="resetState")
+          .forEach((k)=>{
+            this[k]=false;
           });
       },
       false
